acdc36d256ec8bc84888c7dab415148f5cf58774
Denise  Germiniani <111541137+DenuArg@users.noreply.github.com>
2023-09-15 13:00:39 -0300

    Delete Java/Semana 5/estudiantes directory
    No está actualizada

--- Java/Semana 5/estudiantes/src/main/java/utn/estudiantes/repositorio/EstudianteRepositorio.java
+++
@@ -1,7 +0,0 @@
-package utn.estudiantes.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import utn.estudiantes.modelo.Estudiante;
-
-public interface EstudianteRepositorio extends JpaRepository<Estudiante, Integer> {
-}
