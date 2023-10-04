fc67518bf4058374003e7d8d7a64660444326eff
Denise Germiniani <111541137+DenuArg@users.noreply.github.com>
2023-10-04 11:44:06 -0300

    Se Crea Carpeta Semana 7 y Se Modifican Archivos Entorno.js y Monohilo.js

---
+++ Node/Semana 7/01-Hola-Mundo/monohilo.js
@@ -0,0 +1,14 @@
+
+console.log('Hola a toda la cohorte 2022');
+
+var i = 0;
+setInterval(function() {
+    console.log(i);
+    i++;    
+
+    //  if (i === 5) {
+    //     console.log('forzamos un error');
+    //     var a = 3 + z;
+    // }
+}, 1000);
+console.log('Última instrucción');
