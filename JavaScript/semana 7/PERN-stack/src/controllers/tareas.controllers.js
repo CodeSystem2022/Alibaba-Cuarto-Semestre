8b2fbb0fa1a553aeea9fb05fb1cc000da6b146c8
Gabriel <gabrielbeltrando@gmail.com>
2023-10-09 10:17:46 -0300

    actualizacion tarea controllers
    actualizacion tarea controllers

--- JavaScript/semana 7/PERN-stack/src/controllers/tareas.controllers.js
+++ JavaScript/semana 7/PERN-stack/src/controllers/tareas.controllers.js
@@ -1,22 +1,39 @@
 import {pool} from '../db.js';
 
-export const listarTareas = (req, res) => res.send('obteniendo tareas');
+export const listarTareas = async(req, res) => {
 
-export const listarTarea = (req, res) => res.send('obteniendo tarea unica');
-
-export const crearTarea = async (req, res) => {
-    const {titulo, descripcion} = req.body;
+    const resultado = await pool.query('SELECT * FROM tareas');
+    console.log(resultado);
+    return res.json(resultado.rows);    
+}
     
-
-    try {
-        const {rows} = await pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2)',[titulo, descripcion]);
-    console.log(rows);
-    res.send('creando tarea');
-    } catch (error) {
-        console.log("algo salió mal")
-        
+export const listarTarea = async(req, res) => {
+    const resultado = await pool.query('SELECT * FROM tareas WHERE id = $1', [req.params.id]);
+    if (resultado.rowCount === 0){
+        return res.status(404).json({
+            message: 'La tarea no existe'
+        });
     }
-
+    return res.json(resultado.rows(0));
+}
+export const crearTarea = (req, res, next) => {
+    const { titulo, descripcion } = req.body;
+    
+    try {
+           
+        const result = pool.query('INSERT INTO tareas (titulo, descripcion) VALUES ($1, $2)', [titulo, descripcion]);
+        res.json(result.rows[0]);
+        console.log(result.rows[0]);
+    }catch(error){
+        if (error.code === '23505') {
+            return res.status(409).json({
+                message: 'Ya existe una tarea con ese titulo'
+            });
+        }
+        console.log(error);
+        next(error);
+    }
+    
 }
 
 export const actualizarTarea = (req, res) => res.send('actualizando tareas');
