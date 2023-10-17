663435f085d75e9f21dde57f796d053580b29f8a
Sebastián Lerotich <111472465+SebaLerotich@users.noreply.github.com>
2023-10-17 08:26:13 -0300

    Revert "Se realizaron las actividades de los videos 8.1 y 8.2"
    This reverts commit 64b7660577cd4d154fd9ab8f13c856deb20bd1cc.

--- Node/Semana 8/01-Hola-Mundo/app.js
+++
@@ -1,7 +0,0 @@
-const nombre = 'Ariel';
-console.log(nombre);
-
-const nombre2 = 'Juan';
-console.log(nombre2);
-
-console.log('Hola Mundo desde Node');
--- Node/Semana 8/01-Hola-Mundo/app2.js
+++
@@ -1,5 +0,0 @@
-//Creamos una función asignada a una constante
-const saludar = (nombre) =>{ //Función de flecha
-return `Saludos de ${nombre}`; //Se utiliza back tic`s
-}// Esto es equivalente a escribir a 'Saludos + nombre; es decir es una nueva forma
-console.log(saludar('Ariel'));
