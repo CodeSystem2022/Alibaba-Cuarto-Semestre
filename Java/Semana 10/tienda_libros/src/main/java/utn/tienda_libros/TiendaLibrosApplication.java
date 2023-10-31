65b0a8fc4e692071931a6f229c2ef5f6553e42ab
Angel Escobar <111505818+angesc2022@users.noreply.github.com>
2023-10-30 21:28:24 -0300

    se crea la carpeta semana 10
    se realizan las tareas, correspondientes, Dentro de la carpeta Java, crear la carpeta Semana 10 y dentro de la misma copiar los archivos de la semana anterior.
    3-Realizar las siguientes actividades actividades 1 a 5 boton.

---
+++ Java/Semana 10/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -0,0 +1,30 @@
+package utn.tienda_libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.vista.LibroFrom;
+
+import java.awt.EventQueue;
+
+@SpringBootApplication
+public class TiendaLibrosApplication {
+
+	public static void main(String[] args) {
+
+		ConfigurableApplicationContext contextoSpring =
+				new SpringApplicationBuilder(TiendaLibrosApplication.class)
+						.headless(false)
+						.web(WebApplicationType.NONE)
+						.run(args);
+
+		//Ejecutamos el codigo para cargar el formulario
+		EventQueue.invokeLater(() -> { //Metodo Lambda
+			//Obtenemos el objeto from a atraves del spring
+			LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class);
+			libroFrom.setVisible(true);
+		});
+	}
+}
