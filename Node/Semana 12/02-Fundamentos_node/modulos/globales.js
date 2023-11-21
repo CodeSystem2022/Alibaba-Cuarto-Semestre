2ad77175b93ebb0ebd563c0d78143ae84a365ae2
Gabriel <gabrielbeltrando@gmail.com>
2023-11-21 08:32:51 -0300

    Update globales.js
    Vamos a ejecutar varias herramientas del módulo global -> setInterval, setImmediate, require, process...__dirname, __filename

--- Node/Semana 12/02-Fundamentos_node/modulos/globales.js
+++ Node/Semana 12/02-Fundamentos_node/modulos/globales.js
@@ -15,4 +15,20 @@
 //Da prioridad de ejecucion a una funcion asincronica
 //setImmediate(() => {});
 
-console.log(setInterval);
+//console.log(setInterval);
+
+let i = 0;
+let intervalo = setInterval(() =>{
+    console.log('Hola');
+    if (i === 3) {
+        clearInterval(intervalo); // detenemos la funcion
+    }
+    i++;
+},1000);
+
+setInmediate(() =>{
+    console.log('Saludo inmediato');
+});
+//require();
+
+console.log(__filename);
