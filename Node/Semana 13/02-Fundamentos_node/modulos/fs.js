f4b8a5eb2abed60e63b7ebe29f58b290e8553575
Denise  Germiniani <111541137+DenuArg@users.noreply.github.com>
2023-11-27 16:57:54 -0300

    Create fs.js

---
+++ Node/Semana 13/02-Fundamentos_node/modulos/fs.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+//Primero leemos el archivo.txt
+function leer(ruta, cb){
+    fs.readFile(ruta, (err, data) => {
+        cb(data.toString());
+    })
+}
+leer(`${__dirname}/archivo.txt`, console.log); //Sintaxis ES6
